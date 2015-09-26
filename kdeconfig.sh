752fc30919a660ee370d01e7fc93a8bcddb74217
Maciek Krawiec <maciek.krawiec@zoho.com>
2015-09-26 17:41:43 +0200

    Add font entry for windows

--- kdeconfig.sh
+++ kdeconfig.sh
@@ -9,6 +9,7 @@ kwriteconfig5 --file kdeglobals --group General --key menuFont "Open Sans,10,-1,
 kwriteconfig5 --file kdeglobals --group General --key smallestReadableFont "Open Sans,8,-1,5,50,0,0,0,0,0"
 kwriteconfig5 --file kdeglobals --group General --key toolBarFont "Open Sans,9,-1,5,50,0,0,0,0,0"
 kwriteconfig5 --file kdeglobals --group General --key fixed "Iosevka,12,-1,5,50,0,0,0,0,0"
+kwriteconfig5 --file kdeglobals --group WM --key activeFont "Open Sans,10,-1,5,75,0,0,0,0,0"
 
 # KWin
 kwriteconfig5 --file kwinrc --group Compositing --key AnimationSpeed 1
