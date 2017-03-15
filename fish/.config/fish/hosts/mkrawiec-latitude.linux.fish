4e8be47e34b53020989e0e1d3560ec5cc8a512b2
Maciek Krawiec <mkrawiec@strikesocial.com>
2017-03-15 09:22:37 +0100

    Add setup for latitude pc

---
+++ fish/.config/fish/hosts/mkrawiec-latitude.linux.fish
@@ -0,0 +1,8 @@
+# Configuration specific to mkrawiec-latitude hostname
+
+# HDPI fixes
+set -x QT_AUTO_SCREEN_SCALE_FACTOR 1
+set -x GDK_SCALE 1.5
+
+# Virtualfish support
+eval (python3 -m virtualfish)
