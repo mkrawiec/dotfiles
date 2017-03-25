2f554495d17d4b50104d0814aec7ff426131ec62
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-03-25 18:17:02 +0100

    Clear hostname specific configuration

--- fish/.config/fish/hosts/mkrawiec-latitude.linux.fish
+++ fish/.config/fish/hosts/mkrawiec-latitude.linux.fish
@@ -1,8 +1,4 @@
-# Configuration specific to mkrawiec-latitude hostname
-
-# HDPI fixes
-set -x QT_AUTO_SCREEN_SCALE_FACTOR 1
-set -x GDK_SCALE 1.5
+# Configuration specific to mkrawiec-latitude.linux hostname
 
 # Virtualfish support
 eval (python3 -m virtualfish)
---
+++ fish/.config/fish/hosts/szwalnia-thinkpad.linux.fish
@@ -0,0 +1,4 @@
+# Configuration specific to szwalnia-thinkpad.linux hostname
+
+# Virtualfish support
+eval (python3 -m virtualfish)
