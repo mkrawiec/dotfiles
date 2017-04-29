9195325d2d506ef401774a61146b59aff26bd26a
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-04-29 14:56:37 +0200

    Provide install.sh for neovim and tmux

---
+++ neovim/install.sh
@@ -0,0 +1,7 @@
+# install vim plug
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# run :PlugInstall
+nvim -E -c PlugInstall -c qall
+
---
+++ tmux/install.sh
@@ -0,0 +1,3 @@
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
