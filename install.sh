960dd4158a0e58516d1f37e8c252c178eb44e5cf
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-04-30 15:57:46 +0200

    Add checks to install script

--- install.sh
+++ install.sh
@@ -5,13 +5,32 @@ set -e
 declare -r STOW_IGNORE='^stowignore|install.sh$'
 declare -r DOTFILES_ROOT=`dirname $0`
 
+# check if stow is installed
+check_stow()
+{
+    command -v stow >/dev/null 2>&1 || {
+        echo "GNU Stow is required for this script to run" >&2
+        exit 1
+    }
+}
+
+# check provided arguments
+check_args()
+{
+    if [ $# -eq 0 ]; then
+        echo "Choose at least one of the following presets:"
+        ls -d $DOTFILES_ROOT/*/ | sed -e "s/[\.|\/]//g"
+        exit 1
+    fi
+}
+
 # display confirmation prompt
 display_yes_no()
 {
     read -p "Are sure you want to proceed? (y/n)" -n 1 -r
     echo
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        echo "Aborted by user"
+        echo "Aborted by user" >&2
         exit 1
     fi
 }
@@ -29,12 +48,14 @@ run_install_scripts()
         local file="./$f/install.sh"
 
         if [ -f $file ]; then
-            echo "Running install.sh for $f"
+            echo "-- Running install.sh for $f"
             bash "$file" -H || break
         fi
     done
 }
 
+check_stow
+check_args
 display_yes_no
 pushd $DOTFILES_ROOT
     stow_files $@
