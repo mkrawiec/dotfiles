a6412cbf119dc8fe6ca1d338b7e2e38b4e62bb35
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-05-06 16:07:20 +0200

    Make use of Brewfile

--- macos/stowignore/script.d/10-brew.sh
+++ macos/stowignore/script.d/10-brew.sh
@@ -5,11 +5,7 @@ if ! [ -x "$(command -v brew)" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# install packages required for scripts to work
-brew install stow cask
-brew tap caskroom/fonts
-
-# install packages listed in config files
-brew install $(cat $ROOT/brew.list)
-brew cask install $(cat $ROOT/cask.list)
-
+# install brewfile bundle
+pushd $ROOT
+    brew bundle
+popd
