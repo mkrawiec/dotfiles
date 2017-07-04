4086bbaccaa38ab6e43c3859ff940371426b8462
Maciek Krawiec <maciek.krawiec@zoho.com>
2017-07-04 10:03:51 +0200

    Update fish host-specific

--- fish/.config/fish/hosts/mkrawiec-latitude.linux.fish
+++
@@ -1,4 +0,0 @@
-# Configuration specific to mkrawiec-latitude.linux hostname
-
-# Virtualfish support
-eval (python3 -m virtualfish)
---
+++ fish/.config/fish/hosts/mkrawiec-mbp.fish
@@ -0,0 +1,12 @@
+# Configuration specific to mkrawiec-mbp hostname
+
+# Use gnu version of coreutils
+set -U fish_user_paths /usr/local/opt/coreutils/libexec/gnubin /Applications/Postgres.app/Contents/Versions/9.6/bin $fish_user_paths
+set -x MANPATH /usr/local/opt/coreutils/libexec/gnuman:$MANPATH
+
+# Setup links to neovim
+alias vi nvim
+alias vim nvim
+
+# Virtualfish support
+eval (python3 -m virtualfish)
