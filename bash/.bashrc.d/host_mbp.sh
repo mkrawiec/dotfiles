aed49f51bf0674cc4dab0c2ba2c93897a4b9b523
Maciek Krawiec <maciek.krawiec@zoho.com>
2018-05-23 08:41:51 +0200

    Update bash config for macos

--- bash/.bashrc.d/host_mbp.sh
+++ bash/.bashrc.d/host_mbp.sh
@@ -1,4 +1,6 @@
 if [ `hostname` == 'mkrawiec-mbp' ]; then
   export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
   export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+  [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
 fi
