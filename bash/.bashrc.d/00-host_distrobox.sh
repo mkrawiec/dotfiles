534bdd2b72ce61da514c043922adcee600327250
Maciek Krawiec <142805+mkrawiec@users.noreply.github.com>
2023-03-30 23:41:16 +0200

    Improve vscode ergonomics

--- bash/.bashrc.d/00-host_distrobox.sh
+++ bash/.bashrc.d/00-host_distrobox.sh
@@ -1,4 +1,8 @@
 if [[ `hostname` == distrobox-* ]]; then
   export LC_ALL=en_US.UTF8
   eval $(resize)
+
+  export NVM_DIR="$HOME/.config/nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 fi
