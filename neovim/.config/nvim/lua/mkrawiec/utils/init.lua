ad83824d202832f72784bb6a73fee39500e62061
Maciek Krawiec <142805+mkrawiec@users.noreply.github.com>
2025-05-18 19:08:41 +0200

    Add small utils

--- neovim/.config/nvim/lua/mkrawiec/utils/init.lua
+++ neovim/.config/nvim/lua/mkrawiec/utils/init.lua
@@ -10,4 +10,19 @@ function M.reload_config()
   vim.notify("Neovim config reloaded!", vim.log.levels.INFO)
 end
 
+function M.open_config_file(relative_path)
+  local path = vim.fn.stdpath("config") .. "/lua/mkrawiec/" .. relative_path
+  vim.cmd.edit(path)
+end
+
+function M.set_golden_ratio()
+  vim.cmd("wincmd =") -- equalize windows first
+
+  local height = vim.api.nvim_win_get_height(0)
+  local width = vim.api.nvim_win_get_width(0)
+
+  vim.cmd("resize " .. math.floor(height * 5 / 4))
+  vim.cmd("vertical resize " .. math.floor(width * 5 / 4))
+end
+
 return M
