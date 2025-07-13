63f012ff4f82129c35f534563652fe2506563b73
Maciek Krawiec <142805+mkrawiec@users.noreply.github.com>
2025-07-13 14:56:18 +0200

    Add lualine configuration

--- neovim/.config/nvim/lua/mkrawiec/plugins/lualine.lua
+++ neovim/.config/nvim/lua/mkrawiec/plugins/lualine.lua
@@ -1,8 +1,28 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional, for file icons
+  dependencies = { "nvim-tree/nvim-web-devicons" },
   event = "VeryLazy",
   opts = {
+    options = {
+      section_separators = "",
+      component_separators = "│",
+    },
+    sections = {
+      lualine_a = {
+        { "mode", icon = "" },
+      },
+      lualine_b = {
+        { "branch", icon = "", padding = { left = 2, right = 1 } },
+        { "diff", symbols = { added = " ", modified = " ", removed = " " } },
+      },
+      lualine_c = {
+        { "filetype", icon_only = true, colored = false, padding = { left = 1, right = 0 }, separator = "" },
+        "filename",
+      },
+      lualine_x = { "diagnostics" },
+      lualine_y = { "lsp_status" },
+      lualine_z = { { "location", icon = "" } },
+    },
     extensions = {
       "quickfix",
       "fugitive",
